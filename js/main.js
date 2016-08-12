58af25486ad94a4826d5a4a30b7fb5addfe5e0dc
Kevin VanEvery <kvanever@gmail.com>
2016-08-11 18:58:16 -0700

    add jquery toggle for subnav

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  $("li.menu").click(function() {
+    $("nav#sub-navigation").toggle();
+  })
+  $("i.close-menu").click(function() {
+    $("nav#sub-navigation").toggle();
+  })
+})
